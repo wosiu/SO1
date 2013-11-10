6346de778a58f28e583e980d658e4e72f6d9a6c5
Michał Woś <wosiu@students.mimuw.edu.pl>
2013-11-10 12:24:53 +0100

    dodanie flushowanie

--- executor.c
+++ executor.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 
-#define BUF_SIZE 1024
+const int BUF_SIZE = 1024;
+const char EXIT[] = "#exit";
 
 int main(){
 	//char out[BUF_SIZE] = ;
 	char buf[BUF_SIZE];
 	int size;
+	int c = 0;
+	char com[] = "executor";
 
-	if( ( size = read( 0, buf, BUF_SIZE - 1 ) ) == -1 )
-		perror("read");
+	do {
+		strcat( com, " I" );
+		perror( com );
 
-	buf [size < BUF_SIZE - 1 ? size : BUF_SIZE - 1] = '\0';
+		if( ( size = read( 0, buf, BUF_SIZE - 1 ) ) == -1 )
+			perror("read");
 
-	strcat( buf, " executor" );
-	//write( 1, buf, sizeof(buf)-1 );
-	printf("%s",buf);
+		strcat( com, "-" );
+		perror( com );
+
+		buf [size < BUF_SIZE - 1 ? size : BUF_SIZE - 1] = '\0';
+
+		//strcat( buf, " executor" );
+		//write( 1, buf, sizeof(buf)-1 );
+		printf("%s",buf);
+		fflush(stdout);
+		strcat( com, "-" );
+		perror( com );
+
+	} while ( strcmp(buf,EXIT) != 0 );
+	//} while ( 0 );
+
+
+	//while (1) {}
 
 	return 0;
 }
--- manager.c
+++ manager.c
@@ -13,7 +13,7 @@ void syserr( char* msg ) {
 
 int main( int argc, const char* argv[] )
 {
-	int n = 5;
+	int n = 2;
 	char* input_path, output_path;
 	if ( argc > 3 ) {
 		n = atoi( argv[1] );
@@ -22,7 +22,8 @@ int main( int argc, const char* argv[] )
 	}
 
 	//char message[] = "2 7 + 3 / 14 3 - 4 * + 2 /";
-	char message[] = "  jakis napis z macierzystego.. ";
+	char* message = "  jakis napis z macierzystego.. ";
+	//char* message = "#exit";
 
 	// tworzymy lacza dla executerow
 	n++; //bo manager
@@ -33,7 +34,6 @@ int main( int argc, const char* argv[] )
 	}
   	// manager - indeks 0, executerzy [1, n-1]
 
-	printf("przed\n");
 	//przygotowanie łaczy dla executorow
 	for ( i = 1; i < n; i++ ) {
 		switch( fork() ) {
@@ -41,36 +41,49 @@ int main( int argc, const char* argv[] )
 				syserr("Error in fork\n");
 			// w potomku
 			case 0:
-				printf("potomny %d\n",i);
 				// zamiast na in, pojdzie na read do pipe
 				if (dup2(pipe_dsc[ (i-1+n) % n ][0], STDIN_FILENO) == -1) syserr("Error in dup! [0]");
 				// cos zamiast isc na out, bedzie szlo na write do pipe
 				if (dup2(pipe_dsc[i][1], STDOUT_FILENO) == -1) syserr("Error in dup! [1]");
 				execl("./executor", "executor", (char *) 0);
-				//syserr("Error in execl\n");
+				syserr("Error in execl\n");
 			// w macierzystym
 			default:
-				printf("macierzysty %d\n",i);
-				//sprawdzam co dostalem
-
+				{syserr("macierzysty");}
 		}
 	}
 
-	printf("po\n");
 
-	// w managerze nie zastepujemy jego stdin/out. Zna jawnie deskryptory pipe
-	// sluzace do komunikacji z executerami.
-	// manager pisze do pierwszego pipe
-	if ( write (pipe_dsc[0][1], message, sizeof(message) - 1) == -1 )
-		syserr("write");
-	// manager czyta z ostatniego pipe
-	char buf[BUF_SIZE];
-	if ( ( size = read (pipe_dsc[n-1][0], buf, sizeof(buf) - 1) ) == -1 )
-		syserr("read");
+	int tests_no = 2;
 
-	buf [size < BUF_SIZE - 1 ? size : BUF_SIZE - 1] = '\0';
+	// TODO for -> while
+	// wczytujemy kolejne linie testu i puszczamy je w pierscien
+	//
+	for( i = 0; i < tests_no; i++ ) {
+		if ( i == tests_no - 1 ) message = "#exit";
+		// w managerze nie zastepujemy jego stdin/out. Zna jawnie deskryptory pipe
+		// sluzace do komunikacji z executerami.
+		// manager pisze do pierwszego pipe
+		if ( write (pipe_dsc[0][1], message, sizeof(message) - 1) == -1 )
+			syserr("write");
+		// manager czyta z ostatniego pipe
+		char buf[BUF_SIZE];
 
-	printf("%s [koniec]\n",buf);
+		printf("macierzysty napisal\n");
+
+
+		if ( ( size = read (pipe_dsc[n-1][0], buf, sizeof(buf) - 1) ) == -1 )
+			syserr("read");
+
+		printf("macierzysty wczytal\n");
+
+		buf [size < BUF_SIZE - 1 ? size : BUF_SIZE - 1] = '\0';
+
+		printf("%s [koniec]\n",buf);
+	}
+
+
+
 	for ( i = 0; i < n; i++ ) {
 		wait(0);
 	}
